e381b03cc6adbb5a2a381803d9ee5810d217eefb
john cobain <andreygdsn@gmail.com>
2023-11-27 19:52:17 -0300

    calculadora básica

---
+++ modulo-1/0exercicios/ex01-2-calculadora/script.js
@@ -0,0 +1,9 @@
+let x = parseFloat(prompt("Digite um número:"));
+let y = parseFloat(prompt("Digite outro número:"));
+
+let sum = x + y;
+let sub = x - y;
+let times = x * y;
+let divide = x / y;
+
+console.log(`Os números ${x} e ${y} possuem soma ${sum}, subtração ${sub}, multiplicação ${times} e divisão ${divide}`);
