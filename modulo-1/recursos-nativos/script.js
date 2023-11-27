82d8a8e30dfdd2610961cd3289ea818e172c39d5
john cobain <andreygdsn@gmail.com>
2023-11-27 18:42:17 -0300

    recursos nativos do navegador

---
+++ modulo-1/recursos-nativos/script.js
@@ -0,0 +1,12 @@
+let nome = 'John';
+
+console.log(nome);
+console.error(nome);
+console.warn(nome);
+
+// window.alert(nome);
+// nome = window.prompt('Insira o seu nome');
+// let confirmacao = window.confirm('?');
+
+// alert("hello world");
+
