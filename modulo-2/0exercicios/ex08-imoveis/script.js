b222ec57dbbcccffe2170f7b154c20f6323bc407
john cobain <andreygdsn@gmail.com>
2023-12-03 12:52:09 -0300

    ex08 registro de imóveis

---
+++ modulo-2/0exercicios/ex08-imoveis/script.js
@@ -0,0 +1,60 @@
+/*Cadastro de Imóveis
+Escreva um programa em javascript que funcione como um cadastro de imóveis e atenda aos seguintes requisitos:
+Deve ter um menu interativo que sempre é exibido até que o usuário escolha sair.
+O menu interativo deve mostrar no topo a quantidade de imóveis cadastrados.
+O menu deve ter a opção de salvar um imóvel.
+Para salvar um novo imóvel o programa deve pedir as seguintes informações: Nome do proprietário.Quantidade de quartos.Quantidade de banheiros.Se possui garagem.
+
+Nome do proprietário.
+Quantidade de quartos.
+Quantidade de banheiros.
+Se possui garagem.
+
+O menu também deve ter a opção de mostrar todos os imóveis salvos. */
+let imoveis = [];
+
+let opt ='';
+while(opt!==4){
+    opt = parseInt(prompt(`Imóveis cadastrados: ${imoveis.length}
+1 - Salvar um imóvel
+2 - Mostrar imóveis salvos
+3 - Remover um imóvel
+4 - Sair`));
+
+console.clear();
+switch (opt) {
+    case 1:
+        const nome = prompt('Digite o nome do proprietário do imóvel:');
+        const quartos = parseInt(prompt('Quantos quartos há no imóvel?'));
+        const banheiros = parseInt(prompt('Quantos banheiros há no imóvel?'));
+        const garagem = prompt('O imóvel possui garagem?(s/n)').toLowerCase();
+
+        const novoImovel = {nome, quartos, banheiros, garagem};
+        imoveis.push(novoImovel);
+        alert(`Imóvel de ${nome} registrado!`);
+        break;
+    case 2:
+        let mensagem = '';
+        for(i=0;i<imoveis.length;i++){
+            mensagem +=`Imóvel nº ${i+1}
+Propietário(a): ${imoveis[i].nome}
+Quantidade de Quartos: ${imoveis[i].quartos}
+Quantidade de Banheiros: ${imoveis[i].banheiros}
+${imoveis[i].garagem === 's'? 'Possui garagem': 'Não possui garagem'}\n\n`;
+        }
+        console.log(`Imóveis salvos: ${imoveis.length}\n\n` +mensagem);
+        break;
+    case 3:
+        let remover = parseInt(prompt('Qual o número do imóvel que deseja remover:'));
+
+        imoveis.splice(remover-1,1);
+        alert(`Imóvel nº ${remover} removido!`);
+        break;
+    case 4:
+        alert('Encerrando...');
+        break;
+    default:
+        alert('Entrada Inválida!');
+        break;
+}
+}
