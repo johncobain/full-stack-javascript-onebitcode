8c8f539e96fc980f729526506aafbe00fe0e2789
john cobain <andreygdsn@gmail.com>
2023-12-05 22:48:08 -0300

    funções anônimas

---
+++ modulo-2/funcoes-anonimas/script.js
@@ -0,0 +1,26 @@
+// function somar(a,b){
+//     return a+b;
+// }
+
+// let operacao = somar;
+
+// console.log(operacao(4,5));
+
+// operacao = function(a,b){
+//     return a-b;
+// }
+
+// console.log(operacao(4,5));
+
+olaMundo();
+// oiMundo(); //erro
+
+function olaMundo(){
+    console.log("olá mundo");
+}
+
+const oiMundo = function(){
+    console.log('oi mundo');
+}
+
+oiMundo();
