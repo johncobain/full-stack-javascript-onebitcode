7ea8102b17d1b2a3e6c87eab1c33be3e80cacb95
john cobain <andreygdsn@gmail.com>
2024-01-18 23:32:37 -0300

    estatistica na programacao

---
+++ modulo-4/0exercicios/ex14-estatistica/script.js
@@ -0,0 +1,50 @@
+/*​
+
+​
+Moda
+moda(1, 1, 5, 4, 9, 7, 4, 3, 5, 2, 4, 0, 4) === 4
+*/
+
+function media(...n){
+    const sum = n.reduce((accum, num)=> accum+num,0);
+    return sum/n.length;
+}
+
+function mediaPonderada(...num){
+    let len = 0;
+    const sum = num.reduce((accum,nums)=>{
+        len+=nums.p;
+        return accum + (nums.n*nums.p);
+    },0)
+    return sum/len;
+}
+
+function mediana(...n){
+    n.sort((a,b)=>a-b);
+    if(n.length%2 === 0){
+        const sum = n[Math.floor(n.length/2)-1]+n[Math.floor(n.length/2)];
+        return sum/2;
+    }else{
+        return n[Math.round(n.length/2)-1];
+    }
+}
+
+function moda(...n){
+    //[[n,qtd],[n,qtd]]
+    const quantidades = n.map(num=>[
+        num,
+        n.filter(number =>num===number).length
+    ])
+    quantidades.sort((a,b)=> b[1]-a[1])
+
+    return quantidades[0][0];
+}
+
+console.log(media(2, 6, 3, 7, 4));
+console.log(mediaPonderada({ n: 7, p: 2}, { n: 9, p: 5 }, { n: 3, p: 1 }));
+console.log(mediana(2, 4, 5, 7, 42, 99));
+console.log(mediana(15, 14, 8, 7, 3));
+console.log(moda(1, 1, 5, 4, 9, 7, 4, 3, 5, 2, 4, 0, 4));
+
+
+
